2cd2ddb2a87b5fa8dce0d53d742626b3894eaff1
dodo <dojun43@naver.com>
2024-01-16 13:11:48 +0900

    dags_seoul_api_RealtimeCityAir upload

--- dags/dags_seoul_api_RealtimeCityAir.py
+++ dags/dags_seoul_api_RealtimeCityAir.py
@@ -1,16 +1,27 @@
 from airflow import DAG
 import pendulum
 from airflow.operators.python import PythonOperator
+from sensors.seoul_api_hour_sensor import SeoulApiHourSensor
 from operators.seoul_api_to_csv_operator import SeoulApiToCsvOperator
 from hooks.custom_postgres_hook import CustomPostgresHook
 
 with DAG(
     dag_id='dags_seoul_api_RealtimeCityAir',
-    schedule='10 * * * *',
+    schedule='0 * * * *',
     start_date=pendulum.datetime(2023,12,26, tz='Asia/Seoul'),
     catchup=False
 ) as dag:
     '''서울시 권역별 실시간 대기환경 현황'''
+    RealtimeCityAir_status_sensor = SeoulApiHourSensor(
+        task_id='RealtimeCityAir_status_sensor',
+        dataset_nm='RealtimeCityAir',
+        base_dt_col='MSRDT',
+        day_off=0,
+        poke_interval=600,
+        timeout = 600*6,
+        mode='reschedule'
+    )
+
     RealtimeCityAir_status_to_csv = SeoulApiToCsvOperator(
         task_id='RealtimeCityAir_status_to_csv',
         dataset_nm='RealtimeCityAir',
@@ -31,4 +42,4 @@ with DAG(
                 }
     )
 
-    RealtimeCityAir_status_to_csv >> insrt_postgres
+    RealtimeCityAir_status_sensor >> RealtimeCityAir_status_to_csv >> insrt_postgres
